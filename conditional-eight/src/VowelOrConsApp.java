277cb223f41cb200cd2bd19f68ff2f197e6df2de
Gary Henderson <hendergy@gmail.com>
2019-10-04 12:22:22 -0400

    created checkLetter method to accept hard coded input

--- conditional-eight/src/VowelOrConsApp.java
+++ conditional-eight/src/VowelOrConsApp.java
@@ -1,5 +1,42 @@
+import java.util.Objects;
+
 public class VowelOrConsApp {
     public static void main(String[] args) {
-        
+        //if not a letter OR string is > 1, then print an error msg
+        // print vowel or consonant
+
+        String letter = checkLetter("u");
+
+        System.out.println(letter);
+
+    }
+
+    public static String checkLetter(String input){
+        boolean isLetter = false;
+        String test = "";
+        String letters = "";
+        String[] vowels = {"a", "e", "i", "o", "u"};
+
+
+        while(!isLetter){
+            if(input.length() > 1){
+                letters = "Error. Please enter a letter";
+                isLetter = false;
+            }
+            else{
+                for(String l : vowels){
+                    if(Objects.equals(input, l)){
+                        letters = "The letter is a vowel.";
+                        isLetter = true;
+                        break;
+                    }
+                    else{
+                        letters = "The letter is a consonant.";
+                        isLetter = true;
+                    }
+                }
+            }
+        }
+        return letters;
     }
 }
