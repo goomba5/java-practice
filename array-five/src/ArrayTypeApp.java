8fb3dbcc3e573ba7b1b8d1c8c4caa336de0fb5bd
Gary Henderson <hendergy@gmail.com>
2019-10-07 21:50:14 -0400

    initial commit

---
+++ array-five/src/ArrayTypeApp.java
@@ -0,0 +1,13 @@
+public class ArrayTypeApp {
+    public static void main(String[] args) {
+        // write a Java program to test if an array contains a specific value
+
+        String[] stringArray = {"a", "b", "c"};
+        int[] intArray = {1, 2, 3};
+        double[] floatArray = {1.23, 2.22, 34.44};
+
+
+    }
+
+    public static String checkArrayType
+}
