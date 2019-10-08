8c32079a7c474966f2bf8eef04813d603f31425b
Gary Henderson <hendergy@gmail.com>
2019-10-07 22:33:10 -0400

    first solution using getComponentType()

--- array-five/src/ArrayTypeApp.java
+++ array-five/src/ArrayTypeApp.java
@@ -1,13 +1,25 @@
+import java.lang.reflect.Array;
+
 public class ArrayTypeApp {
     public static void main(String[] args) {
         // write a Java program to test if an array contains a specific value
 
         String[] stringArray = {"a", "b", "c"};
+        Class<?> stringType = stringArray.getClass().getComponentType();
+
         int[] intArray = {1, 2, 3};
-        double[] floatArray = {1.23, 2.22, 34.44};
+        Class<?> intType = intArray.getClass().getComponentType();
 
+        double[] doubleArray = {1.23, 2.22, 34.44};
+        Class<?> doubleType = doubleArray.getClass().getComponentType();
 
+        // print result
+        System.out.println("First array is type " + stringType);
+        System.out.println("Second array is type " + intType);
+        System.out.println("Third array is type " + doubleType);
     }
-
-    public static String checkArrayType
+//
+//    public static Class<?> checkArrayType(Array[] array){
+//        return array.getClass().getComponentType();
+//    }
 }
