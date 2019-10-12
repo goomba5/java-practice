4bc086b892013a155e498b27913a3e76be02bd19
Gary Henderson <hendergy@gmail.com>
2019-10-11 22:22:57 -0400

    first pass

---
+++ java-array-eleven/src/ReverseArrayApp.java
@@ -0,0 +1,8 @@
+public class ReverseArrayApp {
+    public static void main(String[] args) {
+        // write a Java program that reverses an array of numbers
+        int[] bunchaNums = {2, 4, 6, 8, 10, 12, 14};
+
+        
+    }
+}
