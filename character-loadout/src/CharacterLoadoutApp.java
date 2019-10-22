6d5abb039cd794506cceb483d8f2fd26eca34760
Gary Henderson <hendergy@gmail.com>
2019-10-22 13:15:34 -0400

    created kinetic weapon object and list

---
+++ character-loadout/src/CharacterLoadoutApp.java
@@ -0,0 +1,35 @@
+import slots.KineticWeapon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CharacterLoadoutApp {
+    public static void main(String[] args) {
+    /*Kinetic Slot */
+    List<KineticWeapon> kineticWeapons = new ArrayList<>();
+
+    KineticWeapon kineticOne = new KineticWeapon(1, "Randy's Throwing Knife",
+            "I'm taking the left flank!", 951,
+            false, false, true);
+    KineticWeapon kineticTwo = new KineticWeapon(2, "Outbreak Perfected",
+                "<NEW DIRECTIVE:> ~~* consume enhance terminate *~~", 949,
+                false, true, false);
+    KineticWeapon kineticThree = new KineticWeapon(3, "Ace of Spades",
+                "Never fold.", 947,
+                false, true, false);
+
+    if(kineticWeapons.size() > 9){
+        System.out.println("Kinetic slot cannot hold anymore weapons.");
+    }
+
+    kineticWeapons.add(kineticOne);
+    kineticWeapons.add(kineticTwo);
+    kineticWeapons.add(kineticThree);
+
+    }
+    public static String checkKineticSlot(KineticWeapon kw){
+        String value = "";
+
+        return value;
+    }
+}
