feddfe6a5b946d1862aeee9dc2ec811534dbbf2d
Gary Henderson <hendergy@gmail.com>
2019-11-29 23:39:34 -0500

    created top level print method displaying lower level return values

--- java-array-ten/src/ArrayMinMaxApp.java
+++ java-array-ten/src/ArrayMinMaxApp.java
@@ -1,5 +1,47 @@
+import java.util.Arrays;
+
 public class ArrayMinMaxApp {
     public static void main(String[] args) {
-        
+        // write a java program to find the maximum and minimum value of an array
+
+        int[] arr = {10, 7, 3, 25, 4, 20};
+        int[] arr1 = {-10, 7, 3, -25, 4, 20};
+        int[] arr2 = {100, 7, -1, 5, 4, 20};
+
+        // Test 1
+        printMinAndMax(arr);
+
+        // Test 2
+        printMinAndMax(arr1);
+
+        // Test 3
+        printMinAndMax(arr2);
+    }
+    public static void printMinAndMax(int[] arr){
+        int maximum = findMaxInteger(arr);
+        int minimum = findMinInteger(arr);
+
+        System.out.println("The smallest number is " + minimum + " and the largest number is " + maximum);
+    }
+
+    public static int findMaxInteger(int[] arr){
+        int max = arr[0];
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static int findMinInteger(int[] arr){
+        int min = arr[0];
+
+        for(int i = 1; i < arr.length - 1; i++){
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        return min;
     }
 }
