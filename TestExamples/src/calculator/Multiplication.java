f1cd53ae9a1fec455683a260e694be24e70adcdb
Gary Henderson <hendergy@gmail.com>
2020-02-20 20:52:10 -0500

    created multiplication method which returns a double

--- TestExamples/src/calculator/Multiplication.java
+++ TestExamples/src/calculator/Multiplication.java
@@ -1,4 +1,9 @@
 package calculator;
 
 public class Multiplication {
+    public double getProduct(double numOne, double numTwo){
+        double result = numOne*numTwo;
+
+        return result;
+    }
 }
